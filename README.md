16d5b64305c965a2e6f8265db6cd2b29058504be
Chao Luo <chao.luo@corigine.com>
2020-12-22 19:54:54 +0800

    [globalmem] Add char device driver of globalmem

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # drivers
 Linux Drivers Study
+
+
+## globalmem device driver
+程序取自宋宝华的《Linux设备驱动详解》，一个虚拟的globalmem设备驱动。
---
+++ globlmem/globlmem.c
@@ -0,0 +1,197 @@
+#include <linux/module.h>
+#include <linux/fs.h>
+#include <linux/init.h>
+#include <linux/cdev.h>
+#include <linux/slab.h>
+#include <linux/uaccess.h>
+
+#define GLOBALMEM_SIZE	0x1000
+#define MEM_CLEAR		0x1
+#define GLOBALMEM_MAJOR	500
+
+static int globalmem_major = GLOBALMEM_MAJOR;
+module_param(globalmem_major, int, S_IRUGO);
+
+struct globalmem_dev {
+	struct cdev cdev;
+	unsigned char mem[GLOBALMEM_SIZE];
+};
+
+struct globalmem_dev *globalmem_devp;
+
+static ssize_t globalmem_read(struct file *filp, char __user *buf, size_t size, loff_t *f_pos)
+{
+	unsigned long p = *f_pos;
+	unsigned int count = size;
+	int ret = 0;
+	struct globalmem_dev *dev = filp->private_data;
+
+	if (p >= GLOBALMEM_SIZE)
+		return 0;
+	if (count > GLOBALMEM_SIZE - p)
+		count = GLOBALMEM_SIZE - p;
+
+	if (copy_to_user(buf, dev->mem + p, count)) {
+		ret = -EFAULT;
+	} else {
+		*f_pos += count;
+		ret = count;
+
+		printk(KERN_INFO "read %u byte(s) from %lu\n", count, p);
+	}
+
+	return ret;
+}
+
+static ssize_t globalmem_write(struct file *filp, const char __user *buf, size_t size, loff_t *f_pos)
+{
+	unsigned long p = *f_pos;
+	unsigned int count = size;
+	int ret = 0;
+	struct globalmem_dev *dev = filp->private_data;
+
+	if (p >= GLOBALMEM_SIZE)
+		return 0;
+	if (count > GLOBALMEM_SIZE - p)
+		count = GLOBALMEM_SIZE - p;
+
+	if (copy_from_user(dev->mem + p, buf, count)) {
+		ret = -EFAULT;
+	} else {
+		*f_pos += count;
+		ret = count;
+
+		printk(KERN_INFO "written %u byte(s) from %lu\n", count, p);
+	}
+	return ret;
+}
+
+static long globalmem_ioctl(struct file *filp, unsigned int cmd, unsigned long arg)
+{
+	struct globalmem_dev *dev = filp->private_data;
+	
+	switch(cmd) {
+	case MEM_CLEAR:
+		memset(dev->mem, 0, GLOBALMEM_SIZE);
+		printk(KERN_INFO "globalmem is set zero\n");
+		break;
+
+	default:
+		return -ENOTTY;
+	}
+
+	return 0;
+}
+
+static loff_t globalmem_llseek(struct file *filp, loff_t offset, int orig)
+{
+	loff_t ret = 0;
+	
+	switch (orig) {
+	case 0: /* seek from start of file*/
+		if (offset < 0) {
+			ret = -EINVAL;
+			break;
+		}
+		if ((unsigned int)offset > GLOBALMEM_SIZE) {
+			ret = -EINVAL;
+			break;
+		}
+		filp->f_pos = (unsigned int)offset;
+		ret = filp->f_pos;
+		break;
+		
+	case 1:/* seek from current position*/
+		if ((filp->f_pos + offset) > GLOBALMEM_SIZE) {
+			ret = -EINVAL;
+			break;
+		}
+		if ((filp->f_pos + offset) < 0) {
+			ret = -EINVAL;
+			break;
+		}
+		filp->f_pos += offset;
+		ret = filp->f_pos;
+		break;
+		
+	default:
+		ret = -EINVAL;
+		break;
+	}
+	return ret;
+}
+
+static int globalmem_open(struct inode *inode, struct file *filp)
+{
+	filp->private_data = globalmem_devp;
+	return 0;
+}
+
+static int globalmem_release(struct inode *inode, struct file *filp)
+{
+	return 0;
+}
+
+static const struct file_operations globalmem_fops = {
+	.owner = THIS_MODULE,
+	.llseek = globalmem_llseek,
+	.read = globalmem_read,
+	.write = globalmem_write,
+	.unlocked_ioctl = globalmem_ioctl,
+	.open = globalmem_open,
+	.release = globalmem_release,
+};
+
+static int globalmem_setup_cdev(struct globalmem_dev *dev, int index)
+{
+	int err;
+	int devno = MKDEV(globalmem_major, index);
+
+	cdev_init(&dev->cdev, &globalmem_fops);
+	dev->cdev.owner = THIS_MODULE;
+	err = cdev_add(&dev->cdev, devno, 1);
+	if (err)
+		printk(KERN_NOTICE "Error %d adding globalmem%d\n", err, index);
+}
+
+static int __init globalmem_init(void)
+{
+	int ret;
+	dev_t devno = MKDEV(globalmem_major, 0);
+
+	if (globalmem_major)
+		ret = register_chrdev_region(devno, 1, "globalmem");
+	else {
+		ret = alloc_chrdev_region(&devno, 0, 1, "globalmem");
+		globalmem_major = MAJOR(devno);
+	}
+	if (ret < 0)
+		return ret;
+
+	globalmem_devp = kzalloc(sizeof(struct globalmem_dev), GFP_KERNEL);
+	if (!globalmem_devp) {
+		ret = -ENOMEM;
+		goto fail_malloc;
+	}
+
+	globalmem_setup_cdev(globalmem_devp, 0);
+	return 0;
+
+fail_malloc:
+	unregister_chrdev_region(devno, 1);
+	return ret;
+}
+module_init(globalmem_init);
+
+static void __exit globalmem_exit(void)
+{
+	dev_t devno = MKDEV(globalmem_major, 0);
+	
+	unregister_chrdev_region(devno, 1);
+	cdev_del(&globalmem_devp->cdev);
+	kfree(globalmem_devp);
+}
+module_exit(globalmem_exit);
+
+MODULE_LICENSE("GPL v2");
+
